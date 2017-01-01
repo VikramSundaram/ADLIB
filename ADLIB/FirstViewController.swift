23db86e27202baf445f022766c10c5411a6f228b
VikramSundaram <govindvikramsundaram@utexas.edu>
2017-01-01 17:09:20 -0600

    Initial Commit

---
+++ ADLIB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ADLIB
+//
+//  Created by Vikram Sundaram on 1/1/17.
+//  Copyright © 2017 VKTRY Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
