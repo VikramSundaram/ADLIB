7b78097e56328b103396a52879a3a8663761e255
VikramSundaram <govindvikramsundaram@utexas.edu>
2017-01-14 21:04:03 -0600

    Built Synth In C with Audio Kit---sustain is buggy when dragging still.

--- ADLIB/SynthScreen.swift
+++ ADLIB/SynthScreen.swift
@@ -8,28 +8,130 @@
 
 import UIKit
 import AVFoundation
+import AudioKit
 
+var synthOne = AKOscillatorBank()
 
 class SynthScreen: UIViewController {
 
-    @IBOutlet var SynthControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        AudioKit.output = synthOne
+        AudioKit.start()
+        synthOne.releaseDuration = 0.35
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func IChord(_ sender: UIButton) {
+    
+    
+    @IBAction func OneStart(_ sender: UIButton) {
         
-        FMSynthesizer.sharedSynth().play(440.0, modulatorFrequency: 679.0, modulatorAmplitude: 0.8)
+        synthOne.play(noteNumber: 60, velocity: 80)
+        synthOne.play(noteNumber: 64, velocity: 100)
+        synthOne.play(noteNumber: 67, velocity: 80)
+    }
+    
+    @IBAction func OneStop(_ sender: UIButton) {
         
+        synthOne.stop(noteNumber: 60)
+        synthOne.stop(noteNumber: 64)
+        synthOne.stop(noteNumber: 67)
         
     }
+    
+    @IBAction func TwoStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 62, velocity: 80)
+        synthOne.play(noteNumber: 65, velocity: 100)
+        synthOne.play(noteNumber: 69, velocity: 80)
+
+    }
+    
+    @IBAction func TwoStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 62)
+        synthOne.stop(noteNumber: 65)
+        synthOne.stop(noteNumber: 69)
+
+    }
+    @IBAction func ThreeStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 64, velocity: 80)
+        synthOne.play(noteNumber: 67, velocity: 100)
+        synthOne.play(noteNumber: 71, velocity: 80)
+    }
+    
+    @IBAction func ThreeStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 64)
+        synthOne.stop(noteNumber: 67)
+        synthOne.stop(noteNumber: 71)
+    }
+  
+    @IBAction func IVStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 65, velocity: 80)
+        synthOne.play(noteNumber: 69, velocity: 100)
+        synthOne.play(noteNumber: 72, velocity: 80)
+    }
+    
+    @IBAction func IVStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 65)
+        synthOne.stop(noteNumber: 69)
+        synthOne.stop(noteNumber: 72)
+    }
+    
+    @IBAction func VStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 67, velocity: 80)
+        synthOne.play(noteNumber: 71, velocity: 100)
+        synthOne.play(noteNumber: 74, velocity: 80)
+    }
+    
+    @IBAction func VStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 67)
+        synthOne.stop(noteNumber: 71)
+        synthOne.stop(noteNumber: 74)
+    }
+    
+    @IBAction func VIStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 69, velocity: 80)
+        synthOne.play(noteNumber: 72, velocity: 100)
+        synthOne.play(noteNumber: 76, velocity: 80)
+
+    }
+    
+    @IBAction func VIStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 69)
+        synthOne.stop(noteNumber: 72)
+        synthOne.stop(noteNumber: 76)
+
+    }
+    
+    @IBAction func VIIStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 71, velocity: 80)
+        synthOne.play(noteNumber: 74, velocity: 100)
+        synthOne.play(noteNumber: 77, velocity: 80)
+    }
+
+    @IBAction func VIIStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 71)
+        synthOne.stop(noteNumber: 74)
+        synthOne.stop(noteNumber: 77)
+
+    }
+    @IBAction func VIIIStart(_ sender: UIButton) {
+        synthOne.play(noteNumber: 72, velocity: 80)
+        synthOne.play(noteNumber: 76, velocity: 100)
+        synthOne.play(noteNumber: 79, velocity: 80)
+    }
+    @IBAction func VIIIStop(_ sender: UIButton) {
+        synthOne.stop(noteNumber: 72)
+        synthOne.stop(noteNumber: 76)
+        synthOne.stop(noteNumber: 79)
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
